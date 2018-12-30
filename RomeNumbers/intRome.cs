4e9d7d25121ebac0d04ec86013e3584d9f896057
Wojciech Wojtkielewicz <wojtkielewicz.wojciech@gmail.com>
2018-12-30 18:43:27 +0100

    Method that converts Arabic (int) to Roman (string) numerals

---
+++ RomeNumbers/intRome.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomeNumbers
+{
+    public class Rome
+    {
+
+        private static int[] arabic = { 1000,100,10,1};
+        private static char[] roman = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+        private static int ROMAN_N = arabic.Length;
+
+        static public string toRome(int a)
+        {
+            StringBuilder s = new StringBuilder();
+
+            if (a < 0 || a>3999)
+                throw new ArgumentOutOfRangeException();
+
+            int ii = 0;
+
+            for (int i = 0; i < 4; i++, ii+=2)
+            {
+                int tmp = a / arabic[i];
+
+                if (1 <= tmp && tmp <= 3)
+                    for (int j = 1; j <= tmp; j++)
+                        s.Append(roman[ii]);
+                else if (tmp == 4)
+                    s.Append(roman[ii]).Append(roman[ii - 1]);
+                else if (tmp == 5)
+                    s.Append(roman[ii - 1]);
+                else if (6 <= tmp && tmp <= 8)
+                {
+                    s.Append(roman[ii - 1]);
+                    for (int j = 1; j <= tmp - 5; j++)
+                        s.Append(roman[ii]);
+                }
+                else if (tmp == 9)
+                    s.Append(roman[ii]).Append(roman[ii - 2]);
+                else
+                    s.Append("");
+
+                a %= arabic[i];
+            }
+
+
+            return s.ToString();
+        }
+
+    }
+}
---
+++ RomeNumbersTests/RomeTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomeNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomeNumbers.Tests
+{
+    [TestClass()]
+    public class RomeTests
+    {
+        [DataTestMethod()]
+        [DataRow("I",1)]
+        [DataRow("II", 2)]
+        [DataRow("III", 3)]
+        [DataRow("IV", 4)]
+        [DataRow("V",5)]    
+        [DataRow("VI", 6)]
+        [DataRow("VII", 7)]
+        [DataRow("VIII", 8)]
+        [DataRow("IX", 9)]
+        [DataRow("X", 10)]
+        [DataRow("XI", 11)]
+        [DataRow("XII", 12)]
+        [DataRow("XIII", 13)]
+        [DataRow("XIV", 14)]
+        [DataRow("XV", 15)]
+        [DataRow("MCMLXXXVI", 1986)]
+        [DataRow("DCCCLX", 860)]
+        [DataRow("CDXXXIX", 439)]
+        [DataRow("MMDCCCLXV", 2865)]
+        [DataRow("MMXVIII", 2018)]
+        [DataRow("MMMCMXCIX", 3999)]
+        public void toRomeTest(string value1, int value2)
+        {
+            Assert.AreEqual(value1, Rome.toRome(value2));
+        }
+    }
+}
